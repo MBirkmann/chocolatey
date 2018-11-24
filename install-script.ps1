0f5f80d235e9a582a3a645b1926b57a6e4334b35
MBirkmann <44998708+MarkusBirkmann@users.noreply.github.com>
2018-11-24 13:41:24 +0100

    Create install-script.ps1

---
+++ install-script.ps1
@@ -0,0 +1,66 @@
+# Install System Utilities
+choco install 7zip -y
+choco install cmdermini -y 
+choco install autoruns -y
+choco install procexp -y
+choco install procmon -y
+choco install freecommander-xe.install-y
+choco install treesizefree -y
+choco install ant-renamer -y
+choco install filezilla -y
+choco install winscp -y
+choco install putty -y
+
+# Install Browsers
+choco install firefox -y 
+choco install googlechrome -y
+
+# Install Media Tools
+choco install eac -y
+choco install vidcutter -y
+choco install h264tscutter -y
+choco install mp3tag -y
+choco install mediathekview -y
+choco install cdburnerxp -y
+
+# Install Media Player / Reader
+choco install vlc -y
+choco install mpc-hc -y 
+choco install kinovea -y 
+choco install sumatrapdf -y
+choco install pdfxchangeeditor -y
+choco install irfanview -y 
+choco install mediainfo -y
+choco install calibre -y 
+
+# Install
+choco install inkscape -y
+choco install pencil -y
+
+# Install Cloud Clients
+choco install nextcloud-client -y
+choco install dropbox -y 
+
+# Install Development Environment
+choco install openjdk -y
+choco install python2 -y
+choco install python3 -y
+choco install maven -y
+choco install intellijidea-ultimate -y
+choco install eclipse -y
+choco install vscode -y
+choco install nodejs -y
+choco install git -y 
+choco install sourcetree -y
+choco install tortoisesvn -y
+choco install soapui -y
+choco install insomnia-rest-api-client -y
+choco install postman -y
+choco install wireshark -y
+choco install fiddler -y
+choco install dbeaver -y
+
+# Install Sport Applications
+choco install garmin-express -y
+choco install r.project -y
+choco install r.studio -y
